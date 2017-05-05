a6b161217e9400db9618f1b856a0ad085ab8e462
Eric Lozano <eric@hotmail.es>
2017-05-05 08:57:23 +0200

    segundo commit

--- src/PracticaGit/w.java
+++ src/PracticaGit/w.java
@@ -12,4 +12,16 @@ package PracticaGit;
  */
 public class w {
 
+    public static void main(String[] args) {
+        System.out.println("HOAL");
+     
+        
+        
+  
+    }
+    
 }
+
+
+
+
